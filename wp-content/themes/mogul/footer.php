e82145a85b8e1430d1db9d45a694c7e477d1ba53
sergey <serjdyachenko@gmail.com>
2018-02-26 23:46:50 +0200

    add style, scripts nd custom logo

--- wp-content/themes/mogul/footer.php
+++ wp-content/themes/mogul/footer.php
@@ -1,34 +1,80 @@
 <?php
 /**
- * The template for displaying the footer
- *
- * Contains the closing of the #content div and all content after.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package mogul
+ * Created by PhpStorm.
+ * User: User
+ * Date: 26.02.2018
+ * Time: 22:36
  */
-
 ?>
+<footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3 footer-side-block" >
+                <div class="footer-side-block__signup">
+                    <h3>
+Sign up for e-mail updates
+</h3>
+                    <p>
+Stay up to date with blah blah for
+    blah blah and yeah blah
+    </p>
+                    <div>
+                        <input style="width:100%" type="text"/>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-5 footer-center-block">
+                <div class="footer-center-block__address">
+                    <h3>
+Mogul Makeup Artistry
+</h3>
+                    <div>
+                        <p>
+Allison Humeniuk
+</p>
+                        <p>
+On-Location Makeup Artist
+</p>
+                    </div>
+                    <ul>
+                        <li><i class="fa fa-map-marker" aria-hidden="true"></i>  Portland, Maine</li>
+                        <li><i class="fa fa-envelope-o" aria-hidden="true"></i>allison@mogulmakeup.com</li>
+                        <li><i class="fa fa-phone" aria-hidden="true"></i>(207)370.4427</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col-md-4 footer-side-block">
+                <div class="footer-side-block__social">
+                    <h3>
+Stay connected with us!
+                    </h3>
+                    <p>Stay up to date with blah blah for</p>
+                    <p>   blah blah and yeah blah</p>
+                    <div class='icon-wrapper'>
+                        <i class="fa fa-lg fa-facebook"></i>
+                    </div>
+                    <div class='icon-wrapper'>
+                        <i class="fa fa-lg fa-twitter"></i>
+                    </div>
+                    <div class='icon-wrapper'>
+                        <i class="fa fa-lg fa-instagram"></i>
+                    </div>
+                    <div class='icon-wrapper'>
+                        <i class="fa fa-lg fa-pinterest-p"></i>
+                    </div>
+                    <div class='icon-wrapper'>
+                        <i class="fa fa-lg fa-linkedin"></i>
+                    </div>
+                    <div class='icon-wrapper'>
+                        <i class="fa fa-lg fa-envelope"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
 
-	</div><!-- #content -->
+    </div>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'mogul' ) ); ?>"><?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'mogul' ), 'WordPress' );
-			?></a>
-			<span class="sep"> | </span>
-			<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'mogul' ), 'mogul', '<a href="http://underscores.me/">Underscores.me</a>' );
-			?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
-
-<?php wp_footer(); ?>
-
+</footer>
+</div>
 </body>
 </html>
